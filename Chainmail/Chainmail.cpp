3aa1239b1e62868caf0261908b10736e43f5ed22
Imod Horvalds <ihorvalds@outlook.com>
2023-02-11 20:39:13 +0000

    Add initial working source.

---
+++ Chainmail/Chainmail.cpp
@@ -0,0 +1,289 @@
+// Chainmail.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <boost/program_options.hpp>
+#include <regex>
+#include <numeric>
+#include <ranges>
+#include <string>
+#include <Windows.h>
+#include <tlhelp32.h>
+
+namespace po = boost::program_options;
+
+bool run_next(const std::string& cmd)
+{
+    STARTUPINFOA si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    if (!CreateProcessA(NULL,
+        (LPSTR)cmd.c_str(),
+        NULL,
+        NULL,
+        FALSE,
+        0,
+        NULL,
+        NULL,
+        &si,
+        &pi))
+    {
+        DWORD dwErr = GetLastError();
+        LPSTR errorText = NULL;
+
+        FormatMessageA(
+            // use system message tables to retrieve error text
+            FORMAT_MESSAGE_FROM_SYSTEM
+            // allocate buffer on local heap for error text
+            | FORMAT_MESSAGE_ALLOCATE_BUFFER
+            // Important! will fail otherwise, since we're not 
+            // (and CANNOT) pass insertion parameters
+            | FORMAT_MESSAGE_IGNORE_INSERTS,
+            NULL,    // unused with FORMAT_MESSAGE_FROM_SYSTEM
+            dwErr,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            (LPSTR)&errorText,  // output 
+            0, // minimum size for output buffer
+            NULL);   // arguments - see note 
+
+        if (errorText != NULL)
+        {
+            std::cerr << errorText << std::endl;
+
+            // release memory allocated by FormatMessage()
+            LocalFree(errorText);
+            errorText = NULL;
+        }
+
+        return false;
+    }
+
+    // Clean up handles
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return true;
+}
+
+/// Wait for processes by handles
+bool wait_for_processes(const std::vector<HANDLE>& handles)
+{
+    bool bResult = true;
+    auto processes = handles | std::views::transform(GetProcessId) | std::views::transform([](const DWORD dw) { return std::to_string((unsigned long)dw); });
+
+    std::cout << "Waiting on processes: "
+        << std::accumulate(processes.begin(), processes.end(), std::string(""), [](const std::string& a, const std::string& b)
+            {
+                if (a.empty())
+                {
+                    return b;
+                }
+
+                if (b.empty())
+                {
+                    return a;
+                }
+                return a + ", " + b;
+            })
+        << "...\n";
+
+    DWORD dwWaitResult = WaitForMultipleObjects((DWORD)handles.size(), handles.data(), TRUE, INFINITE);
+
+    try
+    {
+        if ((dwWaitResult >= WAIT_ABANDONED_0 && dwWaitResult <= (WAIT_ABANDONED_0 + handles.size() - 1)) ||
+            dwWaitResult == WAIT_TIMEOUT || dwWaitResult == WAIT_FAILED)
+        {
+            throw std::string("Wait for processes failed.");
+        }
+
+        std::cout << "Finished waiting for processes.\n";
+    }
+    catch (const std::string& sv)
+    {
+        std::cerr << sv << std::endl;
+        bResult = false;
+    }
+
+    return bResult;
+}
+
+std::vector<HANDLE> get_process_handles_by_glob(const std::vector<std::string>& globs)
+{
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    std::vector<HANDLE> handles;
+
+    if (hSnap == INVALID_HANDLE_VALUE)
+    {
+        DWORD dwErr = GetLastError();
+        LPSTR errorText = NULL;
+
+        FormatMessageA(
+            // use system message tables to retrieve error text
+            FORMAT_MESSAGE_FROM_SYSTEM
+            // allocate buffer on local heap for error text
+            | FORMAT_MESSAGE_ALLOCATE_BUFFER
+            // Important! will fail otherwise, since we're not 
+            // (and CANNOT) pass insertion parameters
+            | FORMAT_MESSAGE_IGNORE_INSERTS,
+            NULL,    // unused with FORMAT_MESSAGE_FROM_SYSTEM
+            dwErr,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            (LPSTR)&errorText,  // output 
+            0, // minimum size for output buffer
+            NULL);   // arguments - see note 
+
+        if (errorText != NULL)
+        {
+            std::cerr << errorText << std::endl;
+
+            // release memory allocated by FormatMessage()
+            LocalFree(errorText);
+            errorText = NULL;
+        }
+
+        return handles;
+    }
+
+    PROCESSENTRY32 pe;
+    ZeroMemory(&pe, sizeof(PROCESSENTRY32));
+    pe.dwSize = sizeof(PROCESSENTRY32);
+    if (!Process32First(hSnap, &pe))
+    {
+        std::cerr << "Failed to read first process. Your system probably has bigger issues. Good luck!\n";
+        return handles;
+    }
+
+    do
+    {
+        char szProcName[256];
+        std::wcstombs(szProcName, pe.szExeFile, 256);
+
+        for (auto& sGlob : globs)
+        {
+            std::regex reg(sGlob, std::regex_constants::ECMAScript | std::regex_constants::icase);
+
+            std::cmatch match;
+
+            if (std::regex_match(szProcName, match, reg))
+            {
+                HANDLE hProc = OpenProcess(SYNCHRONIZE | PROCESS_QUERY_INFORMATION, FALSE, pe.th32ProcessID);
+                if (hProc != NULL)
+                {
+                    handles.push_back(hProc);
+                }
+                else
+                {
+                    const wchar_t szwErr[] = L"Failed to open process ";
+                    char szErr[256];
+
+                    std::wcstombs(szErr, szwErr, 256);
+                    std::cerr << szErr << szProcName << std::endl;
+                }
+            }
+        }
+    } while (Process32Next(hSnap, &pe));
+
+    return handles;
+}
+
+std::vector<HANDLE> get_process_handles_by_pid(const std::vector<unsigned long >& pids)
+{
+    std::vector<HANDLE> handles;
+
+    for (auto pid : pids)
+    {
+        HANDLE hProc = OpenProcess(SYNCHRONIZE | PROCESS_QUERY_INFORMATION, FALSE, pid);
+        if (hProc != NULL)
+        {
+            handles.push_back(hProc);
+        }
+        else
+        {
+            char szErrBuff[256];
+
+            sprintf_s(szErrBuff, "Failed to open process %ul\n", pid);
+            std::cerr << szErrBuff;
+        }
+    }
+
+    return handles;
+}
+
+bool close_handles(const std::vector<HANDLE>& handles)
+{
+    bool bResult = true;
+    for (auto& hProc : handles)
+    {
+        bResult &= (bool)CloseHandle(hProc);
+    }
+
+    return bResult;
+}
+
+int main(int argc, char* argv[])
+{
+    po::options_description options("Available arguments");
+    options.add_options()
+        ("help,h", po::value<std::string>(), "Show this help message")
+        ("pids,ps", po::value<std::vector<unsigned long>>(), "PIDs of processes to wait for")
+        ("procs", po::value<std::vector<std::string>>(), "Names of processes to wait for. Supports globbing")
+        ("next,n", po::value<std::string>(), "Command to run after waited processes exit");
+
+    po::variables_map var_map;
+
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, options), var_map);
+
+        if (var_map.count("help"))
+        {
+            std::cout << options << std::endl;
+            return 0;
+        }
+
+        if (!var_map.count("next"))
+        {
+            std::cout << "Missing command to run after processes end\n";
+            std::cout << options << std::endl;
+            return -1;
+        }
+
+
+        std::vector<HANDLE> handles;
+        if (var_map.count("pids"))
+        {
+            handles = get_process_handles_by_pid(var_map["pids"].as<std::vector<unsigned long>>());
+        }
+        else if (var_map.count("procs"))
+        {
+            handles = get_process_handles_by_glob(var_map["procs"].as<std::vector<std::string>>());
+        }
+        else
+        {
+            std::cerr << "Neither \"pids\", not \"procs\" specified. What are we waiting for?\n";
+            return -1;
+        }
+
+        if (!wait_for_processes(handles))
+        {
+            return -1;
+        }
+
+        return run_next(var_map["next"].as<std::string>()) ? 0 : -1;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << "\n";
+    }
+    catch (...) 
+    {
+        std::cerr << "Exception of unknown type!\n";
+    }
+
+    return -1;
+}
